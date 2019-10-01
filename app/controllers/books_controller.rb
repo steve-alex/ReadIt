1e979bc945e2066287f2277d53474efd4bed7232
steve-alex <steve.alex@hotmail.co.uk>
2019-10-01 13:19:55 +0100

    Minor changes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -61,6 +61,10 @@ class BooksController < ApplicationController
     end
   end
 
+  def search
+    @books = APIRequestMaker.new(params[:search_id], params[:intitle]).build_book_hash
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_book
