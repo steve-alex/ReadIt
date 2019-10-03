19b9a2360b2cbddddd2cfc3bdf2d6ba01666e652
github username <github email address>
2019-10-03 14:22:45 +0100

    fix things, and display routes better

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,27 @@
 Rails.application.routes.draw do
-  resources :users, except: [:destroy]#, except: [:index]
-  resources :reviews, except: [:index, :new, :show]
-  resources :reading_lists, include: [:index, :show, :edit, :update]
+  #, except: [:index]
+  resources :reviews, only: [:update]
 
+  get "books/:id/new_review", to: "reviews#new", as: "new_review"
+  get "reviews/:id/edit", to: "reviews#edit", as: "edit_review"
+  post "reviews", to: "reviews#create"
+  delete "books/:id", to: "reviews#destroy", as: "delete_review"
+
+  resources :reading_lists, include: [:index, :show] #:edit, :update ??
+
+  resources :users, except: [:destroy]
+  delete "users", to: "users#destroy"
+  
+  get 'books/:id', to: 'books#show', as: 'book'
+  get 'books', to: 'books#index', as: 'books' #to delete!!!
+  get "books/:id/reviews", to: "books#reviews", as: "book_reviews"
   post 'books', to: 'books#search', as: 'search_books'
   post 'books/select', to: 'books#select', as: 'select_book'
-  get 'books/:id', to: 'books#show', as: 'book'
-  resources :books, include: [:index]
-  resources :sessions, include: [:new, :create, :destroy]
-  delete "sessions", to: "sessions#destroy"
-  delete "users", to: "users#destroy"
   post "books/archivebook", to: "books#archivebook", as: "add_book"
-  get "books/:id/reviews", to: "books#reviews", as: "book_reviews"
-  get "books/:id/new_review", to: "reviews#new", as: "new_review"
-  
+ 
+
+  get "sessions/new", to: "sessions#new", as: "new_session"
+  post 'sessions', to: "sessions#create"
+  delete "sessions", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
