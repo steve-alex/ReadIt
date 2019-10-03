1eb98d51ffa456bba634c95f9f0794093f303164
github username <github email address>
2019-10-03 17:00:40 +0100

    more clean up and added settings page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :settings]
 
   def index
     @users = User.all
+  end
+
+  def settings
+    
   end
 
   def show
