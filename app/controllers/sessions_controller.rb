7013812ecfb3fe31cb6c5724583c918276b527a5
steve-alex <steve.alex@hotmail.co.uk>
2019-10-04 14:04:39 +0100

    LOADS OF STUFF

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
     def new
+        session[:user_id] = User.new.id
     end
 
     def create
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@ ReadingListBook.destroy_all
 
 
 a = User.create(username: "Steve", password: "Steve")
-ReadingList.create(user_id: a.id, name: "Currently Reading")
-ReadingList.create(user_id: a.id, name: "Finished Reading")
-ReadingList.create(user_id: a.id, name: "Wish List")
+ReadingList.create(user_id: a.id, name: "Currently Reading", deletable: false)
+ReadingList.create(user_id: a.id, name: "Finished Reading", deletable: false)
+ReadingList.create(user_id: a.id, name: "Wish List", deletable: false)
 
 b = User.create(username: "Matt", password:"Matt")
-ReadingList.create(user_id: b.id, name: "Currently Reading")
-ReadingList.create(user_id: b.id, name: "Finished Reading")
-ReadingList.create(user_id: b.id, name: "Wish List")
+ReadingList.create(user_id: b.id, name: "Currently Reading", deletable: false)
+ReadingList.create(user_id: b.id, name: "Finished Reading", deletable: false)
+ReadingList.create(user_id: b.id, name: "Wish List", deletable:false)
